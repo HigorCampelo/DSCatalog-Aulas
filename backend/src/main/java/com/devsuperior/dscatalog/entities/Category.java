1d652e83d885e4d3791bd4164694d0ac858fdc03
Higor Campelo <higorxadrez@hotmail.com>
2022-02-27 03:48:06 -0300

    Auditing Data

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,27 +1,37 @@
 package com.devsuperior.dscatalog.entities;
-import java.io.Serializable;
 
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_category")
-public class Category implements Serializable{
-    
+public class Category implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-    private String name;
-    
-    public Category() {
-    }
-    
-    
+	private String name;
+
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant created_At;
+
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant update_At;
+
+	public Category() {
+	}
+
 	public Category(Long id, String name) {
 		this.id = id;
 		this.name = name;
@@ -30,16 +40,36 @@ public class Category implements Serializable{
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	public Instant getCreatedAt() {
+		return created_At;
+	}
+
+	public Instant getUpdateAt() {
+		return update_At;
+	}
+
+	@PrePersist
+	public void preCreate() {
+        created_At = Instant.now();
+	}
+
+	@PreUpdate
+	public void preUpdate() {
+		update_At = Instant.now();
+	}
 
 	@Override
 	public int hashCode() {
@@ -48,7 +78,6 @@ public class Category implements Serializable{
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -66,8 +95,5 @@ public class Category implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
-    
-    
+
 }
