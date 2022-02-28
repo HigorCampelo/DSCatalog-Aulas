4253d46da24f305cd4a1be073b13d037c531bb2d
Higor Campelo <higorxadrez@hotmail.com>
2022-02-28 18:15:48 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -8,6 +8,8 @@ import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -35,6 +37,13 @@ public class CategoryService {
 		}
 		return listDto;
 		*/
+	}
+	
+	@Transactional(readOnly = true )
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest){
+		Page<Category> list = repository.findAll(pageRequest);
+		
+		return list.map(x -> new CategoryDTO(x));
 	}
 
 	@Transactional(readOnly = true )
